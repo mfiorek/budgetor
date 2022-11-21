a90c28dd91960502f6b6e79fe09f5ec83d37b2ee
fior <marcin.lukasz.fiorek@gmail.com>
2022-11-21 01:08:54 +0100

    MonthSelector styling improvement

--- src/components/MonthSelector.tsx
+++ src/components/MonthSelector.tsx
@@ -34,7 +34,7 @@ const MonthSelector: React.FC<MonthSelectorProps> = ({ transactions, setPeriodSt
             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
           </svg>
         </Listbox.Button>
-        <Listbox.Options className="absolute z-10 mt-2 max-h-60 w-full overflow-auto rounded-md bg-slate-800 py-1">
+        <Listbox.Options className="absolute z-10 mt-2 max-h-60 w-full overflow-auto rounded-md bg-slate-800 p-1">
           {[...monthStrings]
             .sort((a, b) => new Date(b).getTime() - new Date(a).getTime())
             .map((month) => (
@@ -42,7 +42,9 @@ const MonthSelector: React.FC<MonthSelectorProps> = ({ transactions, setPeriodSt
                 key={month}
                 value={month}
                 className={({ active, selected }) =>
-                  `relative z-10 cursor-pointer p-2 text-slate-100 ${active && "bg-slate-700"} ${selected && "cursor-default bg-slate-600 font-bold"}`
+                  `relative z-10 my-1 cursor-pointer rounded px-3 py-1.5 text-slate-100 first:mt-0 last:mb-0 ${active && "bg-slate-700"} ${
+                    selected && "cursor-default bg-slate-600 font-bold"
+                  }`
                 }
               >
                 <span>
