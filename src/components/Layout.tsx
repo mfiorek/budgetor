4189c901a8329ac4566314099646fe1921f8f3ef
fior <marcin.lukasz.fiorek@gmail.com>
2022-11-27 15:02:49 +0100

    Fix Loader alignment

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ const Layout: React.FC<PropsWithChildren> = ({ children }) => {
       <BudgetorHead />
       <main className="flex min-h-screen flex-col">
         <Navbar />
-        <div className="grow bg-slate-800 text-slate-200">
+        <div className="flex grow bg-slate-800 text-slate-200">
           <div className="mx-auto flex w-full flex-col items-center px-2 py-4 lg:max-w-5xl">{children}</div>
         </div>
       </main>
