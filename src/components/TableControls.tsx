f19c46ba021c6dd9df010c87e558b5e9dcfee2cd
fior <marcin.lukasz.fiorek@gmail.com>
2022-12-01 14:25:26 +0100

    Filters applied info change

--- src/components/TableControls.tsx
+++ src/components/TableControls.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Disclosure, Listbox } from "@headlessui/react";
 import { useFieldArray, useForm, useWatch } from "react-hook-form";
-import { useAtom } from "jotai";
+import { useAtom, useSetAtom } from "jotai";
 import { groupColumnsAtom, filterAtom } from "../state/atoms";
 
 const GroupForm = () => {
@@ -123,7 +123,7 @@ const FilterForm = () => {
 };
 
 const TableControls = () => {
-  const [groupColumnsAtomValue, setGroupColumnsAtomValue] = useAtom(groupColumnsAtom);
+  const setGroupColumnsAtomValue = useSetAtom(groupColumnsAtom);
   const [filterAtomValue, setFilterAtomValue] = useAtom(filterAtom);
 
   return (
@@ -143,18 +143,20 @@ const TableControls = () => {
                 </svg>
               )}
             </Disclosure.Button>
-            {(!!groupColumnsAtomValue.length || !!filterAtomValue.length) && !open && (
-              <div className="flex items-center justify-between gap-2 rounded-md border border-amber-500 bg-slate-700 bg-opacity-20 p-1">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className="h-6 w-6 stroke-amber-500">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-                  />
-                </svg>
-                <p>Group or filters applied!</p>
+            {!!filterAtomValue.length && !open && (
+              <div className="flex items-center justify-between gap-4 rounded-md border border-sky-800 bg-slate-700 bg-opacity-20 p-1">
+                <div className="flex gap-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                    />
+                  </svg>
+                  <p>Filters applied</p>
+                </div>
                 <button
-                  className="flex h-8 items-center justify-center rounded bg-red-500 bg-opacity-50 p-2 hover:bg-red-400 hover:bg-opacity-50"
+                  className="flex h-8 items-center justify-center rounded  bg-sky-800 p-2 hover:bg-sky-700"
                   onClick={() => {
                     setGroupColumnsAtomValue([]);
                     setFilterAtomValue([]);
